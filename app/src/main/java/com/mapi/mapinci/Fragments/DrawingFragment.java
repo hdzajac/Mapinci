6b16d68f910f798c4577b81efb432231a16a79af
Nina Pikulska <Nina Pikulska>
2016-12-11 20:40:32 +0100

    sendToServer

--- app/src/main/java/com/mapi/mapinci/Fragments/DrawingFragment.java
+++ app/src/main/java/com/mapi/mapinci/Fragments/DrawingFragment.java
@@ -29,12 +29,24 @@ import com.mapi.mapinci.Utils.graph.NodeFactory;
 import com.mapi.mapinci.Utils.graph.segments.Segment;
 import com.mapi.mapinci.Utils.graph.segments.SegmentFactory;
 
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import static android.R.attr.width;
 import com.loopj.android.http.*;
 import com.mapi.mapinci.Utils.graph.segments.Shape;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.entity.StringEntity;
@@ -42,7 +54,7 @@ import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class DrawingFragment extends Fragment {
 
-    private static final String URL = "http://localhost:8080/coordinate";
+    private static final String URL = "http://10.22.111.252:8080/coordinate";
 
     DrawView drawView;
 
@@ -91,7 +103,9 @@ public class DrawingFragment extends Fragment {
             StringEntity body = new StringEntity(shape.toJson().toString());
 
             AsyncHttpClient client = new AsyncHttpClient();
+
             client.post(getContext(), URL, body, "application/json", new AsyncHttpResponseHandler() {
+
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                     System.out.println("Success! "+statusCode);
@@ -102,7 +116,6 @@ public class DrawingFragment extends Fragment {
                     System.out.println("failure.. "+statusCode+"  "+error.getMessage());
                 }
             });
-
 
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
